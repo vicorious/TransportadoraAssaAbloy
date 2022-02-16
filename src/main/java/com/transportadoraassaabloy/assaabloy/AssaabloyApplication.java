8010c581524aa38199dc5f5152fe8d71368817c1
cristobal.camano <cristobal.camano@jumbotours.com>
2022-02-16 10:52:04 -0500

    Proyecto TransportadoraAssaAbloy

---
+++ src/main/java/com/transportadoraassaabloy/assaabloy/AssaabloyApplication.java
@@ -0,0 +1,13 @@
+package com.transportadoraassaabloy.assaabloy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssaabloyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssaabloyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transportadoraassaabloy/assaabloy/AssaabloyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transportadoraassaabloy.assaabloy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssaabloyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
